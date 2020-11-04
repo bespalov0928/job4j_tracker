5e4ce4cd402783d87aec116339c2c31feb637ba2
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2020-11-04 21:04:34 +0300

    2/2.3/5/4.5. Travis CI и PostgreSQL [#314163]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,4 +1,4 @@
-create table if not exists items (
-   id serial primary key not null,
-   name text
-);
+ create table if not exists items (
+    id serial primary key not null,
+    name text
+ );
